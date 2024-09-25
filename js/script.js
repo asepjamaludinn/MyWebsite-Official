096e9173e9e34dc04fa9b9eca1aa5464ee393341
asepjamaludinn <asepjamaludinn24@gmail.com>
2024-09-25 17:35:32 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,165 @@
+// menu icon
+
+let menuicon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuicon.onclick = () => {
+  menuicon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// scroll
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  // sticky navbar
+  let header = document.querySelector(".header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // hapus navbar sesuai uk media
+  menuicon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// dark mode
+let darkmodeicon = document.querySelector("#darkmode-icon");
+
+darkmodeicon.onclick = () => {
+  darkmodeicon.classList.toggle("bx-sun");
+  document.body.classList.toggle("dark-mode");
+};
+
+// scroll reveal
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading, .quote", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img img, .bar, .portfolio-box, .education-box, .contact form",
+  { origin: "bottom" }
+);
+
+ScrollReveal().reveal(".home-content h1, .about-img img", { origin: "left" });
+ScrollReveal().reveal(".home-content h3, .home-content p, .about-content", {
+  origin: "right",
+});
+
+// form contact
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullName.value} <br> Email: ${email.value} <br> Phone Number: ${phone.value} <br> Message: ${mess.value}`;
+  Email.send({
+    SecureToken: "cc4aef64-aafd-4c1a-a020-1339012aad4c",
+    Host: "smtp.elasticemail.com",
+    Username: "asepjamaludinn24@gmail.com",
+    Password: "EA3D55471FB71BF8F65766BA8928D1623122",
+    To: "asepjamaludinn24@gmail.com",
+    From: "asepjamaludinn24@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully!",
+        icon: "success",
+      });
+    }
+  });
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    if (items[1].value != "") {
+      checkEmail();
+    }
+
+    items[1].addEventListener("keyup", () => {
+      checkEmail();
+    });
+
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+function checkEmail() {
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+  const errorTxtEmail = document.querySelector(".error-txt.email");
+
+  if (!email.value.match(emailRegex)) {
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+
+    if (email.value != "") {
+      errorTxtEmail.innerText = "Enter a valid email address";
+    } else {
+      errorTxtEmail.innerText = "Email Address can't be blank";
+    }
+  } else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  if (
+    !fullName.classList.contains("error") &&
+    !email.classList.contains("error") &&
+    !phone.classList.contains("error") &&
+    !subject.classList.contains("error") &&
+    !message.classList.contains("error")
+  ) {
+    sendEmail();
+
+    form.reset();
+    return false;
+  }
+});
